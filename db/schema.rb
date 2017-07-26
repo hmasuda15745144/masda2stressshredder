63a0c46e32dfb71977f23c945441f3f5fcf64375
QILIGEER <qiligeer0313@gmail.com>
2017-07-26 21:43:26 +0900

    Revert "sign_up and login error is changed"

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724161401) do
+ActiveRecord::Schema.define(version: 20170626112513) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "remember_dig"
-    t.string   "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
