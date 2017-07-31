d9c2a2171a4e2e1769be45a478ca7713ac4419a3
QILIGERU <a1532gq@aiit.ac.jp>
2017-07-31 13:11:31 +0000

    changed sign-up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729080913) do
+ActiveRecord::Schema.define(version: 20170724161401) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20170729080913) do
     t.datetime "updated_at",      null: false
     t.string   "remember_dig"
     t.string   "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
