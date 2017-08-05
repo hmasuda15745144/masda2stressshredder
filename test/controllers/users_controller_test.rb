4472f0af742a4c567b4371403ef245bc7aa45f49
QILIGERU <a1532gq@aiit.ac.jp>
2017-08-05 08:46:28 +0000

    chaneged login/sign-up/remenber

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-     get login_path
+    get users_new_url
     assert_response :success
   end
 
