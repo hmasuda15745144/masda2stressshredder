2d202401406cbec01cf61dccdbdc230ac464a751
prius20 <a1544hm@aiit.ac.jp>
2017-08-11 06:45:21 +0000

    Revert "user cont."
    This reverts commit 3622c051974abd46f879996aba6120ed404dcaa1.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,53 +1,9 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:guchi, :jiman, :inori, :index, :show, :edit, :update, :destroy]
-    
-  def list
-    @msg = 'Users cont. list アクション'
-    @users = User.all
-  end
-
-  def guchi
-    @msg = 'Users cont. guchi アクション'
-    @user = User.find(params[:id])
-  end
-  
-  def guchi_feedback
-    @msg = 'Users cont. guchi_feedback アクション'
-  end
-  
-  def jiman
-    @msg = 'Users cont. jiman アクション'
-    @user = User.find(params[:id])
-  end
-  
-  def jiman_feedback
-    @msg = 'Users cont. jiman_feedback アクション'
-  end
-  
-  def inori
-    @msg = 'Users cont. inori アクション'
-    @user = User.find(params[:id])
-  end
-
-  def inori_feedback
-    @msg = 'Users cont. inori_feedback アクション'
-  end
-  
-  def index
-    @msg = 'Users cont. index アクション'
-    @user = User.find(params[:id])
-  end
   
   def show
-    @msg = 'Users cont. show アクション'
     @user = User.find(params[:id])
-    @micropost = Micropost.find(params[:id])
   end
   
-  def edit
-  @msg = 'Users cont. edit アクション'
-  end
-
   def new
     @user = User.new
   end
@@ -65,12 +21,7 @@ before_action :set_user, only: [:guchi, :jiman, :inori, :index, :show, :edit, :u
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-      @micropost = Micropost.find(params[:id])
-    end
-    
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
