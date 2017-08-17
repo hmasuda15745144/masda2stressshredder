9f02e478e004f9b87d95a8f71ef21b40304a23e9
prius20 <a1544hm@aiit.ac.jp>
2017-08-17 10:16:36 +0000

    routers  mod.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+
+
   get   'list2'      =>'static_pages#list2'
 
   get 'sessions/new'
   get 'users/new'
-  #resources :microposts
+  
+  resources :microposts
   
   #root 'users#index'
   root                'static_pages#home'
